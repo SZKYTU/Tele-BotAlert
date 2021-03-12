87eb337a2366c386c88502137aab716c34bb9c82
Piotr Czupyk <szkycode@gmail.com>
2021-03-12 22:50:01 +0100

    first

---
+++ main.py
@@ -0,0 +1,42 @@
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('asdasdasd')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    updater = Updater("1604309347:AAFYtZnpHhkocmukEwztC6j8yiBi9Fq5Lfc", use_context=True)
+
+    dp = updater.dispa
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ webapi.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+import websockets
+import asyncio
+import time
+import json
+import os
+
+
+load_dotenv()
+
+
+async def cryptocompare():
+    url = os.getenv("URL")
+    async with websockets.connect(url) as websocket:
+        await websocket.send(json.dumps({
+            "action": "SubAdd",
+            "subs": ["2~Coinbase~BTC~USD"],
+        }))
+        while True:
+            try:
+                data = await websocket.recv()
+            except websockets.ConnectionClosed:
+                break
+            try:
+                data = json.loads(data)
+                if len(data) == 16:
+                    BTC_price = data["PRICE"]
+                    crypto_name = data["FROMSYMBOL"]
+                else:
+                    pass
+            except ValueError:
+                print(data)
+    return BTC_price, crypto_name
+
+
+class SocketData:
+    def __init__(self, price, crypto):
+        self.price = cryptocompare.BTC_price
+
+
+asyncio.get_event_loop().run_until_complete(cryptocompare())
