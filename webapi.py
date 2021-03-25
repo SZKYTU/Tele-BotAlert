b9faa09c89496905c6476b06b7f56b9ad00af301
Piotr Czupyk <szkycode@gmail.com>
2021-03-25 18:53:09 +0100

    req

--- webapi.py
+++ webapi.py
@@ -15,21 +15,24 @@ async def cryptocompare():
             "action": "SubAdd",
             "subs": ["2~Coinbase~BTC~USD"],
         }))
-        while True:
-            try:
-                data = await websocket.recv()
-            except websockets.ConnectionClosed:
-                break
-            try:
-                data = json.loads(data)
-                if len(data) == 16:
-                    BTC_price = data["PRICE"]
-                    crypto_name = data["FROMSYMBOL"]
-                else:
-                    pass
-            except ValueError:
-                print(data)
-    return BTC_price, crypto_name
+
+        async def loop():
+            while True:
+                try:
+                    data = await websocket.recv()
+                except websockets.ConnectionClosed:
+                    break
+                try:
+                    data = json.loads(data)
+                    if len(data) == 16:
+                        BTC_price = data["PRICE"]
+                        print(BTC_price)
+                        crypto_name = data["FROMSYMBOL"]
+                    else:
+                        pass
+                except ValueError:
+                    print(data)
+            return BTC_price, crypto_name
 
 
 class SocketData:
